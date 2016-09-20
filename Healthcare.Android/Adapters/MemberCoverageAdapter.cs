ec9992c2d1bce70fd7083a06a9bb2552813c5938
Bizmonger <scott.nimrod@bizmonger.net>
2016-09-19 20:14:00 -0400

    Formatting

--- Healthcare.Android/Adapters/MemberCoverageAdapter.cs
+++ Healthcare.Android/Adapters/MemberCoverageAdapter.cs
@@ -27,6 +27,7 @@ namespace Healthcare.Android.Adapters
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
             var view = convertView ?? _context.LayoutInflater.Inflate(Resource.Layout.MemberCoverageItem, null);
+
             var memberCoverage = this[position];
             var member = memberCoverage.Member;
 
