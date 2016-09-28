d07b21ccdc38e9953ba2e5c91032c941812df517
Bizmonger <scott.nimrod@bizmonger.net>
2016-09-28 03:29:05 -0400

    MemberClaims controls populated

--- Healthcare.Android/Activities/Claims/MemberClaimsActivity.internal.cs
+++ Healthcare.Android/Activities/Claims/MemberClaimsActivity.internal.cs
@@ -1,8 +1,9 @@
-using ManageClaims;
 using Android.Widget;
 using Healthcare.Android.Adapters;
-using static Claims;
+using ManageClaims;
 using System.Collections.Generic;
+using System.Linq;
+using static Claims;
 using static MockMember;
 
 namespace Healthcare.Android
@@ -13,6 +14,26 @@ namespace Healthcare.Android
         {
             _viewModel = new MemberClaimsSummaryViewModel(SomeMemberId, _dispatcher, _repository);
             _viewModel.Load();
+
+            var memberSummary = FindViewById<TextView>(Resource.Id.MemberSummaryValue);
+            memberSummary.Text = _viewModel.Summary.IsSome()
+                ? _viewModel.Summary.Value.Claims.Count().ToString()
+                : "";
+
+            var memberProvidersCharged = FindViewById<TextView>(Resource.Id.MemberProvidersChargedValue);
+            memberProvidersCharged.Text = _viewModel.Summary.IsSome()
+                ? _viewModel.Summary.Value.ProvidersCharged.Item.ToString("C2")
+                : "";
+
+            var MemberInsuranceSavedYou = FindViewById<TextView>(Resource.Id.MemberInsuranceSavedYouValue);
+            MemberInsuranceSavedYou.Text = _viewModel.Summary.IsSome()
+                ? _viewModel.Summary.Value.InsuranceSavings.Item.ToString("C2")
+                : "";
+
+            var memberSaving = FindViewById<TextView>(Resource.Id.MemberSavingValue);
+            memberSaving.Text = _viewModel.Summary.IsSome()
+                ? _viewModel.Summary.Value.TotalSavings().ToString("C2")
+                : "";
 
             var listview = FindViewById<ListView>(Resource.Id.ClaimsListView);
             listview.ChoiceMode = ChoiceMode.Single;
--- ManageClaims/MemberClaimsSummaryViewModel.fs
+++ ManageClaims/MemberClaimsSummaryViewModel.fs
@@ -7,10 +7,11 @@ open Claims
 
 type MemberClaimsSummaryViewModel(memberId , dispatcher:Dispatcher , repository:IClaimsRepository) = 
 
-    member val Summary:ClaimsSummary option = None with get,set
-    member val Claims =  seq [] with get,set
+    member val Summary = None with get,set
+    member val Claims  = seq [] with get,set
 
     member this.Load() =
+
         this.Summary <- repository.GetSummary memberId
         this.Claims  <- match this.Summary with
                         | Some v -> v.Claims
