f0383fcf2df76c1390ce286118ec903ab6074410
Bizmonger <scott.nimrod@bizmonger.net>
2016-11-05 08:35:53 -0400

    Added partial support for Files management

---
+++ Home/Files.fs
@@ -0,0 +1,11 @@
+﻿namespace Home
+
+open Account
+
+type FilesViewModel(patientId:PatientId) =
+
+    member val Files = [] with get,set
+    member val File  = None with get,set
+
+    member this.GetFile() = ()
+    member this.Save()    = ()
---
+++ Tests/_Files.fs
@@ -0,0 +1,21 @@
+﻿module Files
+
+open FsUnit
+open NUnit.Framework
+open Xunit
+open MockMember
+open Home
+
+[<Test>]
+let ``add file`` () =
+
+    // Setup
+    let mutable fileSaved = false
+    let viewModel = FilesViewModel(SomePatientId)
+    viewModel.GetFile()
+
+    // Test
+    viewModel.Save()
+
+    // Verify
+    fileSaved |> should equal true
